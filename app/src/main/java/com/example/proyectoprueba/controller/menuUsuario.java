845c99fed9823015558d68574c7a6a6b0e008432
Anthonyo3477 <anthonyoverdugo55@gmail.com>
2026-07-01 17:37:47 -0400

    solucione el tema de la vista de usuario, es decir toda la vista de usuario esta actualizado como correspode

--- app/src/main/java/com/example/proyectoprueba/controller/menuUsuario.java
+++ app/src/main/java/com/example/proyectoprueba/controller/menuUsuario.java
@@ -7,12 +7,15 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.example.proyectoprueba.R;
 
 public class menuUsuario extends AppCompatActivity {
 
-    private Button btnAvisoProducto, btnAlertas, btnVolver;
+    private CardView cardNuevoAviso;
+    private CardView cardAlertas;
+    private Button btnVolver;
     private ProgressBar progressMenu;
 
     @Override
@@ -20,45 +23,43 @@ public class menuUsuario extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_usuario);
 
-        btnAvisoProducto = findViewById(R.id.btnAvisoProducto);
-        btnAlertas = findViewById(R.id.btnAlertas);
+        cardNuevoAviso = findViewById(R.id.cardNuevoAviso);
+        cardAlertas = findViewById(R.id.cardAlertas);
         btnVolver = findViewById(R.id.btnVolver);
-
         progressMenu = findViewById(R.id.progress_menu);
 
-        // Ir a Productos
-        btnAvisoProducto.setOnClickListener( v ->{
+        // Ir a Agregar Alertas
+        cardNuevoAviso.setOnClickListener(v -> {
             navegar(new Intent(this, agregarAlertas.class));
         });
 
-
-        btnAlertas.setOnClickListener( v ->{
+        // Ir a Notificaciones
+        cardAlertas.setOnClickListener(v -> {
             navegar(new Intent(this, Notificaciones.class));
         });
 
-        // Botón volver al Home
-        btnVolver.setOnClickListener( v ->{
-            finish();
-        });
+        // Volver
+        btnVolver.setOnClickListener(v -> finish());
     }
 
-    private void navegar (Intent intent){
+    private void navegar(Intent intent) {
+
         progressMenu.setVisibility(View.VISIBLE);
 
-        btnAvisoProducto.setEnabled(false);
-        btnAlertas.setEnabled(false);
+        cardNuevoAviso.setEnabled(false);
+        cardAlertas.setEnabled(false);
         btnVolver.setEnabled(false);
-
         startActivity(intent);
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
+
         progressMenu.setVisibility(View.GONE);
 
-        btnAvisoProducto.setEnabled(true);
-        btnAlertas.setEnabled(true);
+        cardNuevoAviso.setEnabled(true);
+        cardAlertas.setEnabled(true);
         btnVolver.setEnabled(true);
     }
 }
